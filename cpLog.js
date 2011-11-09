3546d5c391e6bc117d8bbc1147eeecc6baa0315f
Leon Zinger <leonzinger@gmail.com>
2011-11-09 16:14:43 +0200

    first

---
+++ cpLog.js
@@ -0,0 +1,31 @@
+var cpLog = {
+	init:function(){
+		
+		this.binds();
+	},
+	binds:function(){
+		window.onerror = function(msg,url,num){
+			cpLog.onerror(msg,url,num);
+			return true;
+		}
+	},
+	onerror:function(msg,url,num){
+		this.log.message = msg;
+		this.log.url = url;
+		this.log.num = num;
+		console.log(this.log);
+		this.send();
+	},
+	send:function(){
+		$.ajax({
+			url:'',
+			data:cpLog.log,
+			type:"POST"
+		})
+	},
+	log:{
+		
+	}
+}
+
+cpLog.init();
---
+++ errors.js
@@ -0,0 +1,3 @@
+var error = {
+	number:null
+	
